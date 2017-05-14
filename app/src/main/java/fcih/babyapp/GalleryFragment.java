e492385d43eee2c75b0f19e8631c64fc0fc43f25
nader <nader_alaa@hotmail.com>
2017-05-14 23:03:23 +0200

    Design 1

---
+++ app/src/main/java/fcih/babyapp/GalleryFragment.java
@@ -0,0 +1,36 @@
+package fcih.babyapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import fcih.babyapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GalleryFragment extends Fragment {
+
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    public static GalleryFragment newInstance() {
+        GalleryFragment fragment = new GalleryFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
---
+++ app/src/main/java/fcih/babyapp/HomeFragment.java
@@ -0,0 +1,95 @@
+package fcih.babyapp;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+public class HomeFragment extends Fragment {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ProgressBar mProgressView;
+    private ViewGroup mContainer;
+
+    public HomeFragment() {
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    public void showProgress(final boolean show) {
+        mContainer.setVisibility(show ? View.GONE : View.VISIBLE);
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        getActivity().findViewById(R.id.tabs).setVisibility(View.VISIBLE);
+        getActivity().findViewById(R.id.toolbar).setVisibility(View.VISIBLE);
+        mProgressView = (ProgressBar) getActivity().findViewById(R.id.progress);
+        mContainer = container;
+        showProgress(true);
+        ViewPager mViewPager = (ViewPager) view.findViewById(R.id.container);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.tabs);
+        tabLayout.setVisibility(View.VISIBLE);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setupWithViewPager(mViewPager);
+        return view;
+    }
+
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position){
+                case 0:
+                    return AboutFragment.newInstance();
+                case 1:
+                    return GalleryFragment.newInstance();
+                case 2:
+                    return ProfileFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return "About";
+                case 1:
+                    return "Gallery";
+                case 2:
+                    return "ProfileFragment";
+            }
+            return null;
+        }
+    }
+}
