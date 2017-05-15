2b3fe84b289c75e88acbbbb4c28715040194072d
nader <nader_alaa@hotmail.com>
2017-05-15 06:26:22 +0200

    navigation

--- app/src/main/java/fcih/babyapp/FireBaseHelper.java
+++ app/src/main/java/fcih/babyapp/FireBaseHelper.java
@@ -307,6 +307,14 @@ public class FireBaseHelper {
             this.name = name;
         }
 
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
         public String getCountry() {
             return country;
         }
